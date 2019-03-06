consuldemocracy/consuldemocracy

Display 'validate document' menu item only when applicable
We are trying to avoid Officers from forgetting to click the “Confirm
vote” button, which is necessary to keep track of who has voted a Poll

To do that, we are not displaying the menu item to go back to the next
person that wants to vote, until the Officer clicks the “Confirm Vote”
button or the “The user has decided not to vote” button

Note: Due to mobile version we have duplicate ids, so using classes for
the menu items to hide them without errors

Note2: We are only hidding the menu item, if there are votable polls,
otherwise the “Confirm vote” button does not appear, and there is no
way of going back to help the next person that wants to vote

spec/features/budget_polls/voter_spec.rb
@@ -149,4 +149,49 @@ feature "BudgetPolls" do
     end
 
   end
+
+  context "Side menu" do
+
+    scenario "'Validate document' menu item with votable polls", :js do
+      login_through_form_as_officer(officer.user)
+
+      visit new_officing_residence_path
+      officing_verify_residence
+
+      expect(page).to have_content poll.name
+
+      within("#side_menu") do
+        expect(page).to_not have_content("Validate document")
+      end
+
+      within("#poll_#{poll.id}") do
+        click_button("Confirm vote")
+        expect(page).to have_content "Vote introduced!"
+      end
+
+      within("#side_menu") do
+        expect(page).to have_content("Validate document")
+      end
+    end
+
+    scenario "'Validate document' menu item without votable polls", :js do
+      create(:poll_voter, poll: poll, user: user)
+
+      login_through_form_as_officer(officer.user)
+
+      visit new_officing_residence_path
+      officing_verify_residence
+
+      expect(page).to have_content poll.name
+
+      within("#poll_#{poll.id}") do
+        expect(page).to have_content "Has already participated in this poll"
+      end
+
+      within("#side_menu") do
+        expect(page).to have_content("Validate document")
+      end
+    end
+
+  end
 end

smarques/consul

spec/features/polls/voter_spec.rb
@@ -164,6 +164,48 @@ feature "Voter" do
       expect(Poll::Voter.count).to eq(1)
     end
 
-  end
+    context "Side menu" do
+      scenario "'Validate document' menu item with votable polls", :js do
+        login_through_form_as_officer(officer.user)
 
+        visit new_officing_residence_path
+        officing_verify_residence
+
+        expect(page).to have_content poll.name
+
+        within("#side_menu") do
+          expect(page).not_to have_content("Validate document")
+        end
+
+        within("#poll_#{poll.id}") do
+          click_button("Confirm vote")
+          expect(page).to have_content "Vote introduced!"
+        end
+
+        within("#side_menu") do
+          expect(page).to have_content("Validate document")
+        end
+      end
+
+      scenario "'Validate document' menu item without votable polls", :js do
+        create(:poll_voter, poll: poll, user: create(:user, :in_census))
+
+        login_through_form_as_officer(officer.user)
+
+        visit new_officing_residence_path
+        officing_verify_residence
+
+        expect(page).to have_content poll.name
+
+        within("#poll_#{poll.id}") do
+          expect(page).to have_content "Has already participated in this poll"
+        end
+
+        within("#side_menu") do
+          expect(page).to have_content("Validate document")
+        end
+      end
+
+    end
+  end
 end