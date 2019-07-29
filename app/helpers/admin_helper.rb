rockandror/consuldemocracy

Activate menu entry when administrator is at importation pages

app/helpers/admin_helper.rb
@@ -52,7 +52,7 @@ module AdminHelper
 
   def menu_settings?
     controllers_names = ["settings", "tags", "geozones", "images", "content_blocks",
-      "local_census_records"]
+      "local_census_records", "importations"]
     controllers_names.include?(controller_name) &&
       controller.class.parent != Admin::Poll::Questions::Answers
   end
@@ -72,6 +72,11 @@ module AdminHelper
 
   def menu_dashboard?
     ["actions", "administrator_tasks"].include?(controller_name)
+  end
+
+  def submenu_local_census_records?
+    controller_name == "local_census_records" ||
+    (controller_name == "importations" && controller.class.parent == Admin::LocalCensusRecords)
   end
 
   def official_level_options

smarques/consul

app/helpers/admin_helper.rb
@@ -48,7 +48,7 @@ module AdminHelper
 
   def menu_settings?
     controllers_names = ["settings", "tags", "geozones", "images", "content_blocks",
-      "local_census_records"]
+      "local_census_records", "imports"]
     controllers_names.include?(controller_name) &&
       controller.class.parent != Admin::Poll::Questions::Answers
   end
@@ -68,6 +68,11 @@ module AdminHelper
 
   def menu_dashboard?
     ["actions", "administrator_tasks"].include?(controller_name)
+  end
+
+  def submenu_local_census_records?
+    controller_name == "local_census_records" ||
+    (controller_name == "imports" && controller.class.parent == Admin::LocalCensusRecords)
   end
 
   def official_level_options