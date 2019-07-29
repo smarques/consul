rockandror/consuldemocracy

Allow administrators to run local census records importation
Allow only administrator users to run local census records importation
process

app/models/abilities/administrator.rb
@@ -100,6 +100,7 @@ module Abilities
       can [:manage], Dashboard::AdministratorTask
 
       can :manage, LocalCensusRecord
+      can [:create, :read], LocalCensusRecords::Importation
     end
   end
 end

spec/models/abilities/administrator_spec.rb
@@ -97,4 +97,6 @@ describe Abilities::Administrator do
   it { is_expected.to be_able_to :manage, dashboard_administrator_task }
 
   it { should be_able_to(:manage, LocalCensusRecord) }
+  it { should be_able_to(:create, LocalCensusRecords::Importation) }
+  it { should be_able_to(:show, LocalCensusRecords::Importation) }
 end

spec/models/abilities/everyone_spec.rb
@@ -36,6 +36,8 @@ describe Abilities::Everyone do
   it { should_not be_able_to(:read_results, reviewing_ballot_budget) }
   it { should_not be_able_to(:manage, Dashboard::Action) }
   it { should_not be_able_to(:manage, LocalCensusRecord) }
+  it { should_not be_able_to(:create, LocalCensusRecords::Importation) }
+  it { should_not be_able_to(:show, LocalCensusRecords::Importation) }
 
   context "when accessing poll results" do
     let(:results_enabled) { true }

smarques/consul

app/models/abilities/administrator.rb
@@ -105,6 +105,7 @@ module Abilities
       can [:edit, :update], DownloadSetting
 
       can :manage, LocalCensusRecord
+      can [:create, :read], LocalCensusRecords::Import
     end
   end
 end

spec/models/abilities/administrator_spec.rb
@@ -97,4 +97,6 @@ describe Abilities::Administrator do
   it { is_expected.to be_able_to :manage, dashboard_administrator_task }
 
   it { should be_able_to(:manage, LocalCensusRecord) }
+  it { should be_able_to(:create, LocalCensusRecords::Import) }
+  it { should be_able_to(:show, LocalCensusRecords::Import) }
 end

spec/models/abilities/everyone_spec.rb
@@ -28,6 +28,8 @@ describe Abilities::Everyone do
 
   it { should_not be_able_to(:manage, Dashboard::Action) }
   it { should_not be_able_to(:manage, LocalCensusRecord) }
+  it { should_not be_able_to(:create, LocalCensusRecords::Import) }
+  it { should_not be_able_to(:show, LocalCensusRecords::Import) }
 
   context "when accessing poll results" do
     let(:results_enabled) { true }